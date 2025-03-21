d6b5ccacb923bb440988f1b41dbad8af8c58f4b9
nazmur <nazmur.rashid077@gmail.com>
2025-03-21 03:37:58 -0800

    backend deployed

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
+//  baseURL: "http://localhost:8000/api",
+// https://demo-deploy-api.vercel.app/
+  baseURL: "https://demo-deploy-api.vercel.app/api",
   withCredentials: true,
 });
 
